dab9c6d571ac74a55c66e45753199bbb09bc9800
Vittorio Ballestra <vittorio.ballestra@drafintech.it>
2016-08-07 11:26:07 +0200

    initial

--- lib/index.dart
+++ lib/index.dart
@@ -2,9 +2,35 @@ import 'dart:html';
 import 'dart:js';
 
 
-class TestElement  {
+abstract class TestElement  {
 
+  String get name;
 
+  set name(v);
+
+  var $;
+
+  var properties;
+
+  beforeRegisterDart() {
+    this.properties = new JsObject.jsify({
+      'name': {
+        'type': 'String'
+      }
+    });
+    return "my-tag";
+  }
+
+  void attached() {
+    print("ATTACHED!");
+    name = "pippolo";
+  }
+
+  void clicked(MouseEvent ev,b) {
+    print("CLICKED : ${ev.target} , ${b}");
+    var p = $['ciccio'];
+    name="cicciolo ${b} , ${$}";
+  }
 
 }
 
